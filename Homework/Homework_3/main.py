ef8192d5051891d03c60a25a215b3d55c3a1f5ff
zelen4ik <ytimoshko@mail.ru>
2021-11-06 17:13:30 +0300

    changed file main.py for Homework_3

--- Homework/Homework_3/main.py
+++ Homework/Homework_3/main.py
@@ -127,27 +127,27 @@ currency_convertor = item_2
 #                     31.8 Последним оставить else, при выполнений которого в консоль выведется (“Unknow currency”)
 if currency_convertor:
     currency_usd = usd_item
-    target_currency = input('Введите USD_EUR, USD, RUB, BYN, CHF, UAH:')
+    target_currency = input('Введите USD, EUR, RUB, BYN, CHF, UAH:')
     target_currency_amount = 50
     currency_result = 0
     # print('currency_convertor = ', currency_convertor)
     if target_currency == 'eur':
-        currency_result = target_currency_amount * usd_eur_rate
+        currency_result = target_currency_amount / usd_eur_rate
         print(target_currency_amount, eur_item, '=', currency_result, usd_item)
     elif target_currency == 'uah':
-        currency_result = target_currency_amount * usd_uah_rate
+        currency_result = target_currency_amount / usd_uah_rate
         print(target_currency_amount, uah_item, '=', currency_result, usd_item)
     elif target_currency == 'rub':
-        currency_result = target_currency_amount * usd_rub_rate
+        currency_result = target_currency_amount / usd_rub_rate
         print(target_currency_amount, rub_item, '=', currency_result, usd_item)
     elif target_currency == 'usd':
-        currency_result = target_currency_amount * usd_usd_rate
+        currency_result = target_currency_amount / usd_usd_rate
         print(target_currency_amount, usd_item, '=', currency_result, usd_item)
     elif target_currency == 'chf':
-        currency_result = target_currency_amount * usd_chf_rate
+        currency_result = target_currency_amount / usd_chf_rate
         print(target_currency_amount, chf_item, '=', currency_result, usd_item)
     elif target_currency == 'byn':
-        currency_result = target_currency_amount * usd_byn_rate
+        currency_result = target_currency_amount / usd_byn_rate
         print(target_currency_amount, byn_item, '=', currency_result, usd_item)
 else:
     print('Unknow currency', currency_convertor)
